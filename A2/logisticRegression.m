864dbce7a41c55d9c9fc274adbd9e3dae5f609d5
lcpalmer <lcpalmer@student.cs.uwaterloo.ca>
2017-02-03 21:32:36 -0500

    Logistic Regression

--- A2/logisticRegression.m
+++ A2/logisticRegression.m
@@ -1,4 +1,4 @@
-function [] = mixtureOfGaussians()
+function [] = logisticRegression()
 
 % 10 Data Blocks
 D = cell(10, 1);
@@ -26,14 +26,8 @@ for i = 1:10
   data = [data; D{i}];
   labels = [labels; L{i}];
 end
-[w, w0, pi5, pi6, u5, u6, SIG] = getParameters(data, labels);
-display(w0);
+w = getParameters(data, labels);
 display(w);
-display(strcat('Pi1 = ', num2str(pi5)));
-display(strcat('Pi2 = ', num2str(pi6)));
-display(u5);
-display(u6);
-display(SIG);
 
 % Returns the accuracy of 10-fold cross validation using Mixture of Gaussians.
 function avgAccuracy = crossValidation(D, L)
@@ -50,7 +44,7 @@ function avgAccuracy = crossValidation(D, L)
     end
     testData = D{i};
     testLabels = L{i};
-    acc = applyMOG(data, labels, testData, testLabels);
+    acc = applyLR(data, labels, testData, testLabels);
     result = result + acc;
   end
   avgAccuracy = result / 10;
@@ -58,7 +52,7 @@ function avgAccuracy = crossValidation(D, L)
 % Returns the accuracy of Mixture of Gaussians.
 % Training Set: data & labels.
 % Testing Set: testData & testLabels.
-function accuracy = applyMOG(data, labels, testData, testLabels)
+function accuracy = applyLR(data, labels, testData, testLabels)
   [m,n] = size(data);
   [p,q] = size(labels);
   assert(m == 999);
@@ -73,9 +67,9 @@ function accuracy = applyMOG(data, labels, testData, testLabels)
   assert(d == 1);
   testCount = a;
  
-  [w, w0, pi5, pi6, u5, u6, SIG] = getParameters(data, labels);
+  w = getParameters(data, labels);
   [d1, d2] = size(w);
-  assert(d1 == 1 && d2 == 64);
+  assert(d1 == n+1 && d2 == 1);
  
   correct = 0;
   incorrect = 0;
@@ -83,7 +77,7 @@ function accuracy = applyMOG(data, labels, testData, testLabels)
   for i = 1:testCount
     [d1, d2] = size(testData(i,:));
     assert(d1 == 1 && d2 == 64);
-    prob5 = sig(w*transpose(testData(i,:)) + w0);
+    prob5 = sig(transpose(w)*transpose([1 testData(i,:)]));
     assert(prob5 >= 0 && prob5 <= 1);
     label = testLabels(i, 1);
     assert(label == 5 || label == 6);
@@ -100,58 +94,39 @@ function accuracy = applyMOG(data, labels, testData, testLabels)
 
   accuracy = correct / (correct + incorrect);
 
-function [w, w0, pi5, pi6, u5, u6, SIG] = getParameters(data, labels)
+function w = getParameters(data, labels)
   [m,n] = size(data);
   dataCount = m;
 
-  % Maximum likelihood parameter estimation.
-  N = 0;
-  N5 = 0;
-  N6 = 0;
-  u5 = zeros(1, n);
-  u6 = zeros(1, n);
-  % For each training:
-  for i = 1:dataCount
-    [d1, d2] = size(data(i,:));
-    assert(d1 == 1 && d2 == n);
-    label = labels(i, 1);
-    assert(label == 5 || label == 6);
-    if label == 5
-      N5 = N5 + 1;
-      u5 = u5 + data(i,:);
-    else
-      N6 = N6 + 1;
-      u6 = u6 + data(i,:);
-    end
-    N = N + 1;
-  end
-  u5 = u5 / N5;
-  u6 = u6 / N6;
-  pi5 = N5 / N;
-  pi6 = N6 / N;
-  assert(abs(pi5+pi6-1) < 0.000001);
-  
-  S5 = zeros(n, n);
-  S6 = zeros(n, n);
-  for i = 1:dataCount
-    label = labels(i, 1);
-    assert(label == 5 || label == 6);
-    if label == 5
-      dist = data(i,:)-u5;
-      S5 = S5 + (transpose(dist)*dist);
-    else
-      dist = data(i,:)-u6;
-      S6 = S6 + (transpose(dist)*dist);
-    end
-  end
-  SIG = (S5 + S6) / N;
+  % Logistic regression parameter estimation.
+  w = zeros(n+1, 1);
 
-  % Estimation parameters.
-  w = (u5 - u6) * inv(SIG);
-  [d1,d2] = size(w);
-  assert(d1 == 1 && d2 == n);
-  w0 = -0.5*u5*inv(SIG)*transpose(u5) + 0.5*u6*inv(SIG)*transpose(u6) + log(pi5/pi6);
+  for iterations = 1:10
+    % Calculate the Hessian.
+    H = zeros(n+1, n+1);
+    gradL = zeros(n+1, 1);
+    for i = 1:dataCount
+      d = [1 ; transpose(data(i,:))];
+      [d1, d2] = size(d);
+      assert(d1 == n+1 && d2 == 1);
+      v = sig(transpose(w)*d);
+      H = H + v*(1-v)*d*transpose(d);
+      label = labels(i,1);
+      assert(label == 5 || label == 6);
+      y = tern(label == 5, 1, 0); 
+      gradL = gradL + (v-y)*d;
+    end
+    w = w - inv(H)*gradL;
+  end
 
 % Sigmoid function.
 function s = sig(n)
   s = 1 / (1 + exp(-n));
+
+% Ternary statement.
+function r = tern(b, r1, r2)
+  if (b)
+    r = r1;
+  else
+    r = r2;
+  end
