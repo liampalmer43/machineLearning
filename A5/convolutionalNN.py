cebaef9b0792e7d0c85844b569f7353475aa6822
lucaspalmer8 <lucaspalmer8@gmail.com>
2017-03-27 00:26:17 -0400

    Progress

--- A5/convolutionalNN.py
+++ A5/convolutionalNN.py
@@ -95,7 +95,7 @@ def main(_):
       train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
       print("step %d, training accuracy %.6f"%(i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.75})
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1})
 
   print("test accuracy %.6f"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
